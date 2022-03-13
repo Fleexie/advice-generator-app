e9d9fc2cc700cd903981181ae584050898d25fb2
Fleexie <jannickolsendk@hotmail.com>
2022-03-13 14:19:53 +0100

    Styling Functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-function getAdvice(){
+async function getAdvice(){
+    let advice = await fetch('https://api.adviceslip.com/advice')
+    populateInformation(advice)
+}
+getAdvice();
 
+const adviceID = document.querySelector("#advice_id")
+const adviceTEXT = document.querySelector("#advice_text")
 
+function populateInformation(info){
+    info.json().then(response =>{
+        adviceID.innerHTML = 'Advice #' + response.slip["id"]
+        adviceTEXT.innerHTML = '"' + response.slip["advice"] + '"'
+    })
 }
